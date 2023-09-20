614b309cc8ddff6c35ef7e8bf337b43ae1549377
ROrodrigp <rodrigo.agp96@gmail.com>
2023-09-20 09:35:26 -0600

    Comment code in order to create just one vcn with the vcn module

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "vcn_state" {
-  description = "The state of the VCN"
-  value       = oci_core_vcn.internal.state
-}
+# output "vcn_state" {
+#   description = "The state of the VCN"
+#   value       = oci_core_vcn.internal.state
+# }
 
-output "vcn_cidr" {
-  description = "CIDR block of the core VCN"
-  value       = oci_core_vcn.internal.cidr_block
+# output "vcn_cidr" {
+#   description = "CIDR block of the core VCN"
+#   value       = oci_core_vcn.internal.cidr_block
 
-}
+# }
 
 output "compartment_name" {
   value = oci_identity_compartment.tf-compartment.name
@@ -27,38 +27,56 @@ output "name-of-first-availability-domain" {
   value = data.oci_identity_availability_domains.ads.availability_domains[0].name
 }
 
-# Outputs for compute instance
-output "public-ip-for-compute-instance" {
-  value = oci_core_instance.ubuntu_instance.public_ip
-}
-output "instance-name" {
-  value = oci_core_instance.ubuntu_instance.display_name
-}
+# # Outputs for compute instance
+# output "public-ip-for-compute-instance" {
+#   value = oci_core_instance.ubuntu_instance.public_ip
+# }
+# output "instance-name" {
+#   value = oci_core_instance.ubuntu_instance.display_name
+# }
 
-output "instance-OCID" {
-  value = oci_core_instance.ubuntu_instance.id
-}
+# output "instance-OCID" {
+#   value = oci_core_instance.ubuntu_instance.id
+# }
 
-output "instance-region" {
-  value = oci_core_instance.ubuntu_instance.region
-}
+# output "instance-region" {
+#   value = oci_core_instance.ubuntu_instance.region
+# }
 
-output "instance-shape" {
-  value = oci_core_instance.ubuntu_instance.shape
-}
+# output "instance-shape" {
+#   value = oci_core_instance.ubuntu_instance.shape
+# }
 
-output "instance-state" {
-  value = oci_core_instance.ubuntu_instance.state
-}
+# output "instance-state" {
+#   value = oci_core_instance.ubuntu_instance.state
+# }
 
-output "instance-OCPUs" {
-  value = oci_core_instance.ubuntu_instance.shape_config[0].ocpus
-}
+# output "instance-OCPUs" {
+#   value = oci_core_instance.ubuntu_instance.shape_config[0].ocpus
+# }
 
-output "instance-memory-in-GBs" {
-  value = oci_core_instance.ubuntu_instance.shape_config[0].memory_in_gbs
-}
+# output "instance-memory-in-GBs" {
+#   value = oci_core_instance.ubuntu_instance.shape_config[0].memory_in_gbs
+# }
 
-output "time-created" {
-  value = oci_core_instance.ubuntu_instance.time_created
+# output "time-created" {
+#   value = oci_core_instance.ubuntu_instance.time_created
+# }
+
+# Outputs for the vcn module 
+output "vcn_id" {
+  description = "OCID of the VCN that is created"
+  value = module.vcn.vcn_id
+}
+output "id-for-route-table-that-includes-the-internet-gateway" {
+  description = "OCID of the internet-route table. This route table has an internet gateway to be used for public subnets"
+  value = module.vcn.ig_route_id
+}
+output "nat-gateway-id" {
+  description = "OCID for NAT gateway"
+  value = module.vcn.nat_gateway_id
+}
+output "id-for-for-route-table-that-includes-the-nat-gateway" {
+  description = "OCID of the nat-route table - This route table has a nat gateway to be used for private subnets. This route table also has a service gateway."
+  value = module.vcn.nat_route_id
 }
