6a0287d9402d2a9c00e8742fc2ca94b01b939f4b
rodrigog <rodrigo.ga.garcia@oracle.com>
2023-09-19 19:46:19 -0600

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+compartment_id      = "ocid1.compartment.oc1..aaaaaaaafugnltarxsuulg644fg6z4qhoc5vr74o2eb6cozndmnlc5ekxmqq"
+region              = "us-ashburn-1"
+root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaalbqxbqjg3ac6m73wwtrztfc7yxi3ydgem5ia7qc4vkilfzcaeeaa"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "compartment_id" {
+  description = "OCID from your tenancy page"
+  type        = string
+}
+
+variable "region" {
+  description = "region where you have OCI tenancy"
+  type        = string
+  default     = "us-sanjose-1"
+}
+
+variable "root_compartment_id" {
+  description = "OCID from the root compartment in your tenancy"
+  type        = string
+}
