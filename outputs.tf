fbc6240615e3e39cfbe7f6e4a12b05e24c45fa2a
rodrigog <rodrigo.ga.garcia@oracle.com>
2023-09-19 20:18:18 -0600

    Add name of the availability domain to be used for the compute instance.

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,8 @@ output "compartment_OCID" {
 output "all-availabilty-domains-in-tenancy" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
+
+# The "name" of the availability domain to be used for the compute instance.
+output "name-of-first-availability-domain" {
+  value = data.oci_identity_availability_domains.ads.availability_domains[0].name
+}
