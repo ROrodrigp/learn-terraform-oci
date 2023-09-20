d990beb14352391fee93d54f3ff99ea6312689e0
ROrodrigp <rodrigo.agp96@gmail.com>
2023-09-20 09:44:01 -0600

    Updating code so we can create subnet and compute instance using the vcn from vcn-module.tf

--- main.tf
+++ main.tf
@@ -19,15 +19,15 @@ provider "oci" {
 #   display_name   = "My internal VCN"
 # }
 
-# resource "oci_core_subnet" "dev" {
-#   vcn_id                     = oci_core_vcn.internal.id
-#   cidr_block                 = "172.16.0.0/24"
-#   compartment_id             = var.compartment_id
-#   display_name               = "Dev subnet"
-#   prohibit_public_ip_on_vnic = true
-#   dns_label                  = "dev"
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = module.vcn.vcn_id
+  cidr_block                 = "10.0.0.0/18"
+  compartment_id             = oci_identity_compartment.tf-compartment.id
+  display_name               = "Dev subnet"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
 
-# }
+}
 
 resource "oci_identity_compartment" "tf-compartment" {
   compartment_id = var.root_compartment_id
@@ -35,19 +35,19 @@ resource "oci_identity_compartment" "tf-compartment" {
   name           = "terraform-compartment"
 }
 
-# resource "oci_core_instance" "ubuntu_instance" {
-#   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-#   compartment_id      = var.compartment_id
-#   shape               = "VM.Standard2.1"
-#   source_details {
-#     source_id   = var.compute_image_ocid
-#     source_type = "image"
-#   }
+resource "oci_core_instance" "ubuntu_instance" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = oci_identity_compartment.tf-compartment.id
+  shape               = "VM.Standard2.1"
+  source_details {
+    source_id   = var.compute_image_ocid
+    source_type = "image"
+  }
 
-#   display_name = "ubuntu_instance"
-#   create_vnic_details {
-#     assign_public_ip = false
-#     subnet_id        = oci_core_subnet.dev.id
-#   }
-#   preserve_boot_volume = false
-# }
+  display_name = "ubuntu_instance"
+  create_vnic_details {
+    assign_public_ip = false
+    subnet_id        = oci_core_subnet.dev.id
+  }
+  preserve_boot_volume = false
+}
